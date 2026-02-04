1aca2d96b2df036ae58639e75fa6c5be2a5df441
CaduGiannini <148394698+CaduGiannini@users.noreply.github.com>
2026-02-04 16:48:53 -0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,22 @@
+const calendario = document.getElementById("calendario");
 
+// gerar janeiro simples (exemplo)
+function criarCalendario() {
+  const diasSemana = ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"];
+
+  diasSemana.forEach(d => {
+    const div = document.createElement("div");
+    div.className = "dia titulo";
+    div.innerText = d;
+    calendario.appendChild(div);
+  });
+
+  for (let dia = 1; dia <= 31; dia++) {
+    const div = document.createElement("div");
+    div.className = "dia";
+    div.innerText = dia;
+    calendario.appendChild(div);
+  }
+}
+
+criarCalendario();
